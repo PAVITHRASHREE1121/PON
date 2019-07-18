01e905125aa83471f49d0fbd70fb8fd6929ef677
PAVITHRASHREE1121 <52962742+PAVITHRASHREE1121@users.noreply.github.com>
2019-07-18 14:52:08 +0530

    Create class leap_year.java

---
+++ class leap_year.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class leap_year {
+public static void main(String[] args) {
+	Scanner s=new Scanner (System.in);
+	System.out.println("enter the year");
+	int year=s.nextInt();
+	if(year%4==0)
+	{
+		System.out.println("the given year is leap year");
+	}
+	else
+	{
+		System.out.println("the given year is not leap year");
+	}
+	s.close();
+}
+}
