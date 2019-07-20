da857603c6ab9bbeaee421d2d2221d6a64db06ac
PAVITHRASHREE1121 <52962742+PAVITHRASHREE1121@users.noreply.github.com>
2019-07-20 12:10:40 +0530

    Create  palindrome_number .java

---
+++ palindrome_number .java
@@ -0,0 +1,25 @@
+package prp;
+import java.util.Scanner;
+public class palindrome_number {
+public static void main(String[] args) {
+	Scanner s=new Scanner (System.in);
+	System.out.println("enter the number");
+	int n=s.nextInt();
+	int temp=n; 
+	int sum=0;
+		while(n>0)
+		{
+        int r=n%10;
+	    sum=(sum*10+r);
+		n=n/10;
+		}
+	if (sum==temp)
+	{
+		System.out.println("the given number is palindrome");
+	}
+	else
+	{
+		System.out.println("the given number is not palindrome");
+	}
+	s.close();
+	
