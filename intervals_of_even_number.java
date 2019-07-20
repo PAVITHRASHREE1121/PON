0695ef6d07bcd7cad08b8d8f0f1d6a47e033beb5
PAVITHRASHREE1121 <52962742+PAVITHRASHREE1121@users.noreply.github.com>
2019-07-20 16:10:39 +0530

    Update and rename intervals_of_prime_number.java to intervals_of_even_number.java

--- intervals_of_even_number.java
+++ intervals_of_even_number.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class intervals_of_prime_number {
+public class intervals_of_even_number {
 public static void main(String[] args) {
 	Scanner s=new Scanner (System.in);
 	System.out.println("enter the number");
