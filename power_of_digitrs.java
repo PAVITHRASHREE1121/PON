421b6619758a131ffe3aef6b399a9cd77a9577e5
PAVITHRASHREE1121 <52962742+PAVITHRASHREE1121@users.noreply.github.com>
2019-07-20 11:27:40 +0530

    Update power_of_digitrs.java

--- power_of_digitrs.java
+++ power_of_digitrs.java
@@ -5,9 +5,7 @@ public static void main(String[] args) {
 	Scanner s=new Scanner (System.in);
 	int n=s.nextInt();
 	int k=s.nextInt();
-	
-		 int p=(int)Math.pow(n,k);	
-	
+          int p=(int)Math.pow(n,k);	
 	 System.out.println("the value is  "+ p);
 }
 }
