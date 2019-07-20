7fa2a7c5275f4f9dc296be61536f3bfd3232e20e
PAVITHRASHREE1121 <52962742+PAVITHRASHREE1121@users.noreply.github.com>
2019-07-20 11:26:56 +0530

    Create  power_of_digitrs.java

---
+++ power_of_digitrs.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class power_of_digitrs {
+public static void main(String[] args) {
+	Scanner s=new Scanner (System.in);
+	int n=s.nextInt();
+	int k=s.nextInt();
+	
+		 int p=(int)Math.pow(n,k);	
+	
+	 System.out.println("the value is  "+ p);
+}
+}
