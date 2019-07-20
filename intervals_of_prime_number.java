f4196be6a1bcd29caaa2a913aa8cfa77ec091558
PAVITHRASHREE1121 <52962742+PAVITHRASHREE1121@users.noreply.github.com>
2019-07-20 16:09:43 +0530

    Create  intervals_of_prime_number.java

---
+++ intervals_of_prime_number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class intervals_of_prime_number {
+public static void main(String[] args) {
+	Scanner s=new Scanner (System.in);
+	System.out.println("enter the number");
+	int n=s.nextInt();
+	int m=s.nextInt();
+	for (int i=n+1;i<m;i++)
+	{
+		if(i%2==0)
+		{
+			System.out.println(i);
+		}
+	}
+	s.close();
+}
+}
